5ca2f799d4d9de7356a2ce233727e232ef7a6f38
David9486 <davidraja76399@gmail.com>
2026-04-08 15:05:46 +0530

    api setup and testing

---
+++ src/main/java/com/zoho/nl_sql/NlSqlApplication.java
@@ -0,0 +1,13 @@
+package com.zoho.nl_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlSqlApplication.class, args);
+	}
+
+}
